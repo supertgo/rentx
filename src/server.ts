6e2e7fbd3037732ef2be0fd0226c3c055c33b72b
supertgo <thiagoroberto2003@gmail.com>
2021-12-18 08:32:52 -0300

    add crete(post) and list(get) methods from category to swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 /* eslint-disable no-console */
 import express from 'express';
+
+import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
+
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 

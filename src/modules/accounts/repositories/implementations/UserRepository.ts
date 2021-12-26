ec5e17519bbd6f06b677790ec67020c0f82d0c0b
supertgo <thiagoroberto2003@gmail.com>
2021-12-26 07:36:35 -0300

    feat(users):

---
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    email,
+    driver_license,
+    password
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      driver_license,
+      password
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import { container } from 'tsyringe';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
 import { ICategoryRepository } from '../../modules/cars/repositories/ICategoryRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UserRepository';
 
 container.registerSingleton<ICategoryRepository>(
   'CategoriesRepository',
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );

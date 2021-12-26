c9d0dc8c166ca7f781107f1832c864d2478c6505
supertgo <thiagoroberto2003@gmail.com>
2021-12-26 07:17:59 -0300

    feat(users): :sparkles: create user migration and entitie

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidV4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdimin: boolean;
+
+  @CreateDateColumn()
+  created_at: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };

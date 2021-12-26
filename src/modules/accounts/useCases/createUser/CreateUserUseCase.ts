c572fae592b4ce897f39837b0bdd21f2a41dbe04
supertgo <thiagoroberto2003@gmail.com>
2021-12-26 07:58:18 -0300

    feat(users): :sparkles: create users controller
    create users controller

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'tsyringe';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject('UsersRepository')

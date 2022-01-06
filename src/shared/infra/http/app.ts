b781e464ef7af8420e9eaf7590aec3e354945255
supertgo <thiagoroberto2003@gmail.com>
2022-01-06 10:19:00 -0300

    docs: :memo: protect category routes with auth
    protect category routes with auth at swagger json file

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,9 +4,9 @@ import createConnection from '@shared/infra/typeorm';
 import '../../container';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-
 import { AppError } from '@shared/errors/AppError';
 import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
 
 createConnection();
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerUi));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
